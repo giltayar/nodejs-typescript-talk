569161c452d33ec2bdca439ef215faec5f741e39
Gil Tayar <giltayar@microsoft.com>
2025-05-18 19:32:11 +0300

    01 01 wpi

---
+++ 01-what-is-typescript/01-a-language.ts
@@ -0,0 +1,19 @@
+interface Complex {
+  real: number
+  imaginary: number
+}
+
+export function add(a: Complex, b): Complex {
+  return { real: a.real + b.real, imaginary: a.imaginary + b.imaginary }
+}
+
+export function addMany(...args: Complex[]): Complex {
+  return args.reduce(
+    (acc, curr) => add(acc, curr, 0),
+    { real: 0, imaginary: 0 }
+  )
+}
+
+export function dotProduct(a: Complex, b: Complex): number {
+  return a.real * b.real + a.imaginary * b.imaginary
+}
