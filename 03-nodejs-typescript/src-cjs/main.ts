21749e8ef47bb28ba085a865786a755a8431e4ff
Gil Tayar <giltayar@microsoft.com>
2025-05-20 21:20:14 +0300

    03 mostly done

---
+++ 03-nodejs-typescript/src-cjs/main.ts
@@ -0,0 +1,3 @@
+const { addMany } = require('./complex.ts')
+
+console.log(addMany({ real: 1, imaginary: 2 }, { real: 3, imaginary: 4 }))
---
+++ 03-nodejs-typescript/src/main.ts
@@ -0,0 +1,3 @@
+import { addMany } from './complex.ts'
+
+console.log(addMany({ real: 1, imaginary: 2 }, { real: 3, imaginary: 4 }))
